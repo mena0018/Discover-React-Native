cdcc08f8c046e9704b4d43238bf29d1db8090bfa
mena0018 <rabie.menad@etudiant.univ-reims.fr>
2022-12-12 21:50:12 +0100

    Establishment of the cat generation system on click

--- App.tsx
+++ App.tsx
@@ -1,10 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, Image, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [catImg, setCatImg] = useState(
+    "https://purr.objects-us-east-1.dream.io/i/lcwJe.jpg"
+  );
+
+  const getCat = () => {
+    fetch("https://aws.random.cat/meow")
+      .then((resp) => resp.json())
+      .then((data) => setCatImg(data.file));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Cat Generator</Text>
+      <Image style={styles.img} source={{ uri: catImg }}></Image>
+      <Button onPress={getCat} title="New Cat" color="#841584" />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +26,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f1f1f1",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 20,
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textTransform: "uppercase",
+  },
+  img: {
+    width: "80%",
+    height: "70%",
+    borderRadius: 10,
   },
 });
